7832499e6314d67742fbe69565c9b5b31ae5adb3
Aaron Van Curen <aaronvancuren@gmail.com>
2021-03-01 16:24:23 -0500

    added post/comment/reply classes

---
+++ HalicarnassusChat.Data/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalicarnassusChat.Data
+{
+    public class Comment
+    {
+        [Key]
+        int Id { get; set; }
+
+        [Required]
+        string Text { get; set; }
+
+        [Required]
+        Guid Author { get; set; }
+
+        public virtual List<Reply> Replies { get; set; }
+
+        [Required]
+        public int PostId { get; set; }
+        [ForeignKey(nameof(PostId))]
+        public virtual Post Post { get; set; }
+
+    }
+}
